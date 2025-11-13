77cf0efa8fbf481196aa42f49eafc5a1ba31034c
ATATC <futerry@outlook.com>
2025-11-13 13:14:55 -0500

    Routine updates.

--- main.py
+++ main.py
@@ -10,13 +10,36 @@ if __name__ == "__main__":
     safe_stress = (6, 30)
     safety_factors_top = []
     safety_factors_bot = []
+    shear_forces = []
+    bending_moments = []
+    x = bridge.x_linespace()
     for i in range(1200 - 960):
-        bridge.move_the_train(1)
+        shear_forces.append(bridge.expanded_shear_forces(x))
+        bending_moments.append(bridge.expanded_bending_moments(x))
         sft, sfb = bridge.safety_factor((6, 30))
         safety_factors_top.append(sft)
         safety_factors_bot.append(sfb)
         if i == 172:
             print(bridge.safety_factor(safe_stress))
+        bridge.move_the_train(1)
+    shear_force_envelope = np.max(np.array(shear_forces), axis=0)
+    plt.figure(figsize=(12, 6))
+    plt.plot(x, shear_force_envelope)
+    plt.grid(True)
+    plt.title("Shear Force Envelope")
+    plt.xlabel("Position (mm)")
+    plt.ylabel("Max Shear Force (N)")
+    plt.show()
+    plt.close()
+    bending_moment_envelope = np.max(np.array(bending_moments), axis=0)
+    plt.figure(figsize=(12, 6))
+    plt.plot(x, bending_moment_envelope)
+    plt.grid(True)
+    plt.title("Bending Moment Envelope")
+    plt.xlabel("Position (mm)")
+    plt.ylabel("Max Bending Moment (N)")
+    plt.show()
+    plt.close()
     print(intervals(np.array(safety_factors_top) < 1))
     print(intervals(np.array(safety_factors_bot) < 1))
     plt.plot(safety_factors_top, "orange")
