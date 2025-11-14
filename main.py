8f8a206a8c2903d57730bc98978dd134ff80e055
ATATC <futerry@outlook.com>
2025-11-13 20:36:01 -0500

    Routine updates.

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from initialization import *
 
 if __name__ == "__main__":
     evaluator = Evaluator(bridge, 6, 30, 4)
-    evaluator.set_to_minimal()
-    evaluator.plot_safety_factors()
+    print(evaluator.dead_zones())
     print(evaluator.maximum_load())
     if GRAPH:
         evaluator.plot_safety_factors()
