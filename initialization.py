4dd5493c3ae194b1934d4673e9c3ea393b84d351
ATATC <futerry@outlook.com>
2025-11-13 22:05:33 -0500

    Routine updates.

--- initialization.py
+++ initialization.py
@@ -1,7 +1,7 @@
 from bridge import CIV102Beam, BeamBridge
 
-GRAPH = True
-cross_section = CIV102Beam()
+GRAPH = False
+cross_section = CIV102Beam(top=800)
 bridge = BeamBridge(400, cross_section, load_distribution=(1,) * 6)  # load case 1
 # bridge = BeamBridge(452, cross_section) # load case 2 - first pass
 # bridge = BeamBridge(
