2c4a41f230910bf48ff17da1fb146110a5b1b5d3
ATATC <futerry@outlook.com>
2025-11-17 14:55:12 -0500

    Routine updates.

--- optimize_cross_section.py
+++ optimize_cross_section.py
@@ -1,26 +1,28 @@
 from bridger import *
 from initialization import *
 
+MATBOARD_WIDTH: float = 508
+
 
 def constraint(kwargs: dict[str, float]) -> dict[str, float] | None:
     top, bottom, height = kwargs["top"], kwargs["bottom"], kwargs["height"]
     kwargs["thickness"] = 1.27
     used = top + bottom + 2 * (height - 2.54)
-    if used > 406.5 or top < bottom:
+    if used > MATBOARD_WIDTH or top < bottom:
         return None
-    kwargs["outreach"] = .5 * (406.5 - used)
+    kwargs["outreach"] = .5 * (MATBOARD_WIDTH - used)
     return kwargs if 2 * kwargs["outreach"] < bottom else None
 
 
 def optimize_cross_section() -> None:
     evaluator = Evaluator(bridge, Material())
     optimizer = BeamOptimizer(evaluator)
-    cs, load = optimizer.optimize_cross_section({
-        "top": (10, 406.5, 4),
-        "bottom": (10, 406.5, 4),
-        "height": (10, 406.5, 4),
+    cross_section, load = optimizer.optimize_cross_section({
+        "top": (100, MATBOARD_WIDTH, 1),
+        "bottom": (10, MATBOARD_WIDTH, 1),
+        "height": (20, 200, 20),
     }, independent_params=("top", "bottom", "height"), constraint=constraint)
-    print(cs.kwargs(), load)
+    print(cross_section.kwargs(), load)
 
 
 if __name__ == "__main__":
