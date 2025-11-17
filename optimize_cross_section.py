9bed306bff1cf0e3db384868a2c212d80f2b606c
ATATC <futerry@outlook.com>
2025-11-17 13:20:51 -0500

    Routine updates.

--- optimize_cross_section.py
+++ optimize_cross_section.py
@@ -2,10 +2,25 @@ from bridger import *
 from initialization import *
 
 
+def constraint(kwargs: dict[str, float]) -> dict[str, float] | None:
+    top, bottom, height = kwargs["top"], kwargs["bottom"], kwargs["height"]
+    kwargs["thickness"] = 1.27
+    used = top + bottom + 2 * (height - 2.54)
+    if used > 406.5 or top < bottom:
+        return None
+    kwargs["outreach"] = .5 * (406.5 - used)
+    return kwargs if 2 * kwargs["outreach"] < bottom else None
+
+
 def optimize_cross_section() -> None:
     evaluator = Evaluator(bridge, Material())
     optimizer = BeamOptimizer(evaluator)
-    print(optimizer.optimize_cross_section())
+    cs, load = optimizer.optimize_cross_section({
+        "top": (10, 406.5, 4),
+        "bottom": (10, 406.5, 4),
+        "height": (10, 406.5, 4),
+    }, independent_params=("top", "bottom", "height"), constraint=constraint)
+    print(cs.kwargs(), load)
 
 
 if __name__ == "__main__":
