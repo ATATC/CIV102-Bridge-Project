32794047e9fb247c6fd793d35c766bf744f6611f
ATATC <futerry@outlook.com>
2025-11-19 12:31:55 -0500

    Routine updates.

---
+++ design0.py
@@ -0,0 +1,10 @@
+from bridger import *
+
+cross_section = CIV102Beam()
+bridge = BeamBridge(400, cross_section, load_distribution=(1,) * 6)
+material = Material()
+print(bridge.safety_factor((material.compressive_strength, material.tensile_strength)))
+print(bridge.shear_safety_factor(material.shear_strength))
+print(bridge.glue_safety_factor(material.glue_strength))
+print(bridge.flexural_buckling_safety_factor(bridge.safe_flexural_buckling_stress(material)))
+print(bridge.shear_buckling_safety_factor(bridge.safe_shear_buckling_stress(material)))
