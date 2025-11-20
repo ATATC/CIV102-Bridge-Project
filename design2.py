b80d497b92165ee25ee03ebe3cbffe7e27b3505e
ATATC <futerry@outlook.com>
2025-11-20 15:21:46 -0500

    Routine updates.

--- design2.py
+++ design2.py
@@ -1,20 +1,24 @@
 from bridger import *
 
 material = Material(length_between_stiffeners=125)
-params = {'top': 100.0, 'bottom': 61.0, 'thickness': 1.27, 'outreach': 5}
+params = {'top': 100.0, 'bottom': 60.0, 'thickness': 1.27, 'outreach': 5}
 
-max_height = 200
-min_height = 140
+max_height = 172
+min_height = 160
 
 
 def cross_section(x: float) -> CrossSection:
-    return CIV102Beam(**params, height=max_height - ((max_height - min_height) / 625 ** 2) * (x - 625) ** 2)
+    if x <= 60 or x >= 1250 - 60:
+        return CIV102Beam(**params, height=min_height)
+    # return CIV102Beam(**params, height=max_height - ((max_height - min_height) / 625) * abs(x - 625 - 60))
+    return CIV102Beam(**params, height=max_height - ((max_height - min_height) / 625 ** 2) * (x - 625 - 60) ** 2)
 
 
-bridge = VaryingBeamBridge(1000, cross_section, length=1250, load_distribution=(1.518, 1.518, 1, 1, 1.1, 1.1))
+bridge = VaryingBeamBridge(1500, cross_section, length=1250, load_distribution=(1.518, 1.518, 1, 1, 1.1, 1.1))
 
 if __name__ == "__main__":
     bridge.plot_curvature_diagram(material)
     evaluator = Evaluator(bridge, material)
     print(evaluator.maximum_load())
+    print(bridge.train_load())
     evaluator.plot_safety_factors()
