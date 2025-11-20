1cc8e04db9050f00abf551ee2e793b7ec29be68e
ATATC <futerry@outlook.com>
2025-11-20 15:53:04 -0500

    Routine updates.

--- design1.py
+++ design1.py
@@ -9,8 +9,8 @@ def cross_section(x: float) -> CrossSection:
     return cross_section2 if 416 < x < 833 else cross_section1
 
 
-uniform_bridge = BeamBridge(1000, cross_section1, length=1250, load_distribution=(1.518, 1.518, 1, 1, 1.1, 1.1))
-bridge = VaryingBeamBridge(1000, cross_section, length=1250, load_distribution=(1.518, 1.518, 1, 1, 1.1, 1.1))
+uniform_bridge = BeamBridge(1000, cross_section1, length=1250, load_distribution=(1,) * 6)
+bridge = VaryingBeamBridge(1000, cross_section, length=1250, load_distribution=(1,) * 6)
 
 if __name__ == "__main__":
     uniform_bridge.plot_curvature_diagram(material)
