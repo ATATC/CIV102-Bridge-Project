6ac92a18945f60876cb14c416cea47ea6611ef46
ATATC <futerry@outlook.com>
2025-11-20 16:12:33 -0500

    Routine updates.

--- design2.py
+++ design2.py
@@ -3,7 +3,7 @@ from bridger import *
 material = Material(length_between_stiffeners=125)
 params = {'top': 100.2, 'bottom': 60.7, 'thickness': 1.27, 'outreach': 5}
 
-max_height = 166
+max_height = 166.5
 min_height = 160
 
 
