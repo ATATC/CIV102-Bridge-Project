747a66389402eab3da0ddf30f0986c28e060406d
ATATC <futerry@outlook.com>
2025-11-20 10:53:43 -0500

    Routine updates.

--- final.py
+++ final.py
@@ -8,9 +8,10 @@ def cross_section(x: float) -> CrossSection:
     return cross_section2 if 400 < x < 800 else cross_section1
 
 uniform_bridge = BeamBridge(1000, cross_section1, length=1250, load_distribution=(1.518, 1.518, 1, 1, 1.1, 1.1))
-uniform_bridge
+uniform_bridge.plot_curvature_diagram(material)
 
 bridge = NonUniformBeamBridge(1000, cross_section, length=1250, load_distribution=(1.518, 1.518, 1, 1, 1.1, 1.1))
+bridge.plot_curvature_diagram(material)
 evaluator = Evaluator(bridge, material)
 print(evaluator.maximum_load())
 evaluator.plot_safety_factors()
