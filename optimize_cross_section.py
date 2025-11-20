ad2d0b73fcb293bb7a767cada510ba0064149871
ATATC <futerry@outlook.com>
2025-11-20 16:28:26 -0500

    Routine updates.

--- optimize_cross_section.py
+++ optimize_cross_section.py
@@ -8,7 +8,7 @@ def constraint(kwargs: dict[str, float]) -> dict[str, float] | None:
     top, bottom, height = kwargs["top"], kwargs["bottom"], kwargs["height"]
     kwargs["thickness"] = 1.27
     kwargs["outreach"] = 5
-    used = top + bottom + 2 * (height - 2.54) + 10
+    used = bottom + 2 * (height - 2.54) + 10
     return kwargs if used <= MATBOARD_WIDTH and top > bottom else None
 
 
@@ -19,7 +19,7 @@ def optimize_cross_section() -> None:
         "top": (100, MATBOARD_WIDTH, .1),
         "bottom": (10, MATBOARD_WIDTH, .1),
         "height": (20, 200, 20),
-    }, constraint=constraint, popsize=30, mutation=(.5, 1), recombination=.7, maxiter=2000)
+    }, constraint=constraint)
     print(params, load)
 
 
