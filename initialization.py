ea9d2fbe9bd971489e7f3e1f40405ed55aacc621
ATATC <futerry@outlook.com>
2025-11-20 15:39:05 -0500

    Routine updates.

--- initialization.py
+++ initialization.py
@@ -7,5 +7,5 @@ material = Material(length_between_stiffeners=125)
 # bridge = BeamBridge(452, cross_section)  # load case 2 - first pass
 # bridge = BeamBridge(
 #     1000, cross_section, load_distribution=(1.518, 1.518, 1, 1, 1.1, 1.1), length=1250
-# ) # load case 2 - subsequent passes
+# )  # load case 2 - subsequent passes
 bridge = BeamBridge(400, cross_section, load_distribution=(1,) * 6, length=1250)
