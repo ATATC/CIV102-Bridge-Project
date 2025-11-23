e9f7906d197aea6f1bab5da769b472a3cd33fb6b
ATATC <futerry@outlook.com>
2025-11-23 17:02:18 -0500

    Routine updates.

--- optimize_cross_section.py
+++ optimize_cross_section.py
@@ -1,15 +1,14 @@
 from bridger import *
 from initialization import *
 
-MATBOARD_WIDTH: float = 400
+MATBOARD_WIDTH: float = 450
 
 
 def constraint(kwargs: dict[str, float]) -> dict[str, float] | None:
-    top, bottom, height = kwargs["top"], kwargs["bottom"], kwargs["height"]
     kwargs["thickness"] = 1.27
-    kwargs["outreach"] = 5
-    used = bottom + 2 * (height - 2.54) + 10
-    return kwargs if used <= MATBOARD_WIDTH and top > bottom else None
+    kwargs["outreach"] = 28
+    used = kwargs["bottom"] + 2 * (kwargs["height"] - 2.54) + 2 * kwargs["outreach"]
+    return kwargs if used <= MATBOARD_WIDTH and kwargs["top"] > kwargs["bottom"] else None
 
 
 def optimize_cross_section() -> None:
@@ -18,7 +17,7 @@ def optimize_cross_section() -> None:
     params, load = optimizer.optimize_cross_section({
         "top": (100, MATBOARD_WIDTH, .1),
         "bottom": (10, MATBOARD_WIDTH, .1),
-        "height": (20, 200, 20),
+        "height": (20, 200, 20)
     }, constraint=constraint)
     print(params, load)
 
